a010760007240d6c9d5f8f98895837cfba12b03d
Max <starosvetskiym@gmail.com>
2019-01-21 23:49:33 +0300

    Тестовый ИИ
    Пробная версия. не совсем готовая но работает без багов вроде. Перебрал код Enemy и group_enemy. В остальных файлах значимых изменений нет.

--- Char.gd
+++ Char.gd
@@ -17,34 +17,44 @@ func dead_playr():
 	G.HP_Char -= global_enemy.damage_enemy
 	print(G.HP_Char)
 	
+func move():
+	if Input.is_action_pressed("ui_right"):
+		if is_attaking == false:
+			velocity.x = Speed
+			$AnimatedSprite.play("Run")
+			$AnimatedSprite.flip_h = false
+			if sign($Ray_raiht.position.x) == -1:
+				$Ray_raiht.position.x *= -1
+				$Ray_raiht.cast_to.x *= -1
+	elif Input.is_action_pressed("ui_left"):
+		if is_attaking == false:	
+			velocity.x = -Speed
+			$AnimatedSprite.play("Run")
+			$AnimatedSprite.flip_h = true
+			if sign($Ray_raiht.position.x) == 1:
+				$Ray_raiht.position.x *= -1
+				$Ray_raiht.cast_to.x *= -1
+	else:
+		velocity.x = 0
+		if is_attaking == false:	
+			$AnimatedSprite.play("idle")
+			#$Swing.disabled = true
+			
+	if Input.is_action_pressed("ui_up"):
+		if is_attaking == false:	
+			if onground == true:
+				velocity.y = Jump_P
+				onground = false
+				if velocity.y < 0:
+					$AnimatedSprite.play("jump")
+				else:
+					$AnimatedSprite.play("fall")
+
+
 
 func _physics_process(delta):
 	if dead_playr == false and G.HP_Char > 0:
-		if Input.is_action_pressed("ui_right"):
-			if is_attaking == false:
-				velocity.x = Speed
-				$AnimatedSprite.play("Run")
-				$AnimatedSprite.flip_h = false
-		elif Input.is_action_pressed("ui_left"):
-			if is_attaking == false:	
-				velocity.x = -Speed
-				$AnimatedSprite.play("Run")
-				$AnimatedSprite.flip_h = true
-		else:
-			velocity.x = 0
-			if is_attaking == false:	
-				$AnimatedSprite.play("idle")
-				#$Swing.disabled = true
-			
-		if Input.is_action_pressed("ui_up"):
-			if is_attaking == false:	
-				if onground == true:
-					velocity.y = Jump_P
-					onground = false
-					if velocity.y < 0:
-						$AnimatedSprite.play("jump")
-					else:
-						$AnimatedSprite.play("fall")
+		move()
 		time_cooldown += delta
 		if Input.is_action_just_pressed("ui_focus_next") and is_attaking == false and time_cooldown > 0.6 and G.MP_Char > 0:
 			if is_on_floor():
@@ -57,13 +67,6 @@ func _physics_process(delta):
 				if enemy.has_method('dead'):
 					enemy.damage()
 					G.MP_Char -= 10 # Вычитание стамины при атаке
-					print (1)
-			if $Ray_left.is_colliding() :
-				var enemy2 = $Ray_left.get_collider()
-				if enemy2.has_method('dead'):
-					enemy2.damage()
-					G.MP_Char -= 10 # Вычитание стамины при атаке
-					print(2)
 			time_cooldown = 0
 			
 		
--- Enemy_lvl1.gd
+++ Enemy_lvl1.gd
@@ -10,6 +10,31 @@ var HP_enemy = 100
 var is_attaking = false
 var time_cooldown_enemy = 0
 var time_dead_enemy = 0
+var taking_damag = false
+var in_fight = false
+var Char_detektor = false
+
+func move():
+	velocity.x = Speed * direction
+			
+	if direction == 1: # Меняет направленность анимации бега
+		$AnimatedSprite.flip_h = false
+		
+	else:
+		$AnimatedSprite.flip_h = true
+			
+	
+	$AnimatedSprite.play("run")
+	velocity.y += Gravity	
+	velocity = move_and_slide(velocity, Floor)
+	
+	if $RayCast2D.is_colliding() == false : # Столкновение с обрывом 
+		direction = direction * -1  
+		$RayCast2D.position.x *= -1
+		$Ray_enemy.position.x  *= -1
+		$Ray_enemy.cast_to  *= -1
+		$Detector.position.x  *= -1
+		$Detector.cast_to  *= -1		
 
 func dead(): # Убивает врага
 	$AnimatedSprite.play("dead")
@@ -18,64 +43,77 @@ func dead(): # Убивает врага
 	$run.disabled = true
 	$Timer_dead.start()
 	
-	
 func damage():
 	HP_enemy -= G.damage_Char
+	in_fight = true
+	is_attaking = true
+	taking_damag = true
+	if HP_enemy > 0 and taking_damag:
+		velocity = Vector2(0, 0)
+		$AnimatedSprite.play("taking_damage")
 	if HP_enemy == 0:
 		dead()
-	
+		
+func attake(ray_ataking):
+	if  is_attaking == false:
+		time_cooldown_enemy = 0
+		if is_on_floor():
+			velocity.x = 0
+			is_attaking = true
+			in_fight = true
+		var Char = ray_ataking.get_collider()
+		if Char.has_method('dead_playr'):
+			$AnimatedSprite.play("swing")
+			if taking_damag == false:
+				Char.dead_playr()
 
+func char_detector(Ray_enemy, Char_detect):
+	if Ray_enemy.is_colliding() : #Атака по герою если он стоит слева
+		Char_detect = Ray_enemy.get_collider()
+		if Char_detect.has_method('dead_playr'):
+			Char_detect = true
+			return Char_detect
+		else:
+			Char_detect = false
+			return Char_detect
+	else:
+		Char_detect = false
+		return Char_detect
+	
 
 func _physics_process(delta):
 	if is_dead == false and HP_enemy > 0:
-		time_cooldown_enemy += delta
-		if $Ray_right_enemy.is_colliding() and is_attaking == false and time_cooldown_enemy > 1.5: #Атака по герою если он стоит справа 
-			time_cooldown_enemy = 0
-			if is_on_floor():
-				velocity.x = 0
-				is_attaking = true
-			var Char = $Ray_right_enemy.get_collider()
-			if Char.has_method('dead_playr'):
-				$AnimatedSprite.flip_h = false
-				$AnimatedSprite.play("swing")
-				Char.dead_playr()
-				
-				
-		if $Ray_left_enemy.is_colliding() and is_attaking == false and time_cooldown_enemy > 1.5: #Атака по герою если он стоит слева
-			time_cooldown_enemy = 0
-			if is_on_floor():
-				velocity.x = 0
-				is_attaking = true
-			var Char2 = $Ray_left_enemy.get_collider()
-			if Char2.has_method('dead_playr'):
-				$AnimatedSprite.flip_h = true
-				$AnimatedSprite.play("swing")
-				Char2.dead_playr()
-				
-		if is_attaking == false: 
-			velocity.x = Speed * direction
+		#time_cooldown_enemy += delta
+		
+							
+		if is_attaking == false and in_fight == false:
+			move()
+			if is_on_wall() : # Столкновение со стенами 
+				direction = direction * -1 
+				$RayCast2D.position.x *= -1 # Разворачивает RayCast2D при повороте спрайта
+				$Ray_enemy.position.x  *= -1
+				$Ray_enemy.cast_to  *= -1
+				$Detector.position.x  *= -1
+				$Detector.cast_to  *= -1	
+		if $Detector.is_colliding():
+			if char_detector($Detector, Char_detektor):
+				in_fight = true
+		else:
+			in_fight = false
 			
-			if direction == 1: # Меняет направленность анимации бега
-				$AnimatedSprite.flip_h = false
+		if in_fight == true and is_attaking == false:
+			move()
+			if $Ray_enemy.is_colliding():
+				attake($Ray_enemy)
 			else:
-				$AnimatedSprite.flip_h = true
+				in_fight = false
+		
 			
-			$AnimatedSprite.play("run")
-			velocity.y += Gravity	
-			velocity = move_and_slide(velocity, Floor)
-			
-			if is_on_wall(): # Столкновение со стенами 
-				direction = direction * -1 
-				$RayCast2D.position.x *= -1 # Разворачивает RayCast2D при повороте спрайта 
 				
-			if $RayCast2D.is_colliding() == false: # Столкновение с обрывом 
-				direction = direction * -1 
-				$RayCast2D.position.x *= -1
-		
-		
 
 func _on_AnimatedSprite_animation_finished(): # даёт возможность проиграть анимацию атаки
 	is_attaking = false
+	taking_damag = false
 	
 
 
